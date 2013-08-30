5d6f64a652994af4cf1a56fcf2babf21a8d6d52f
lauragarcia <lauragarcia1993@hotmail.com>
2013-08-30 15:52:55 -0500

    Add a link to Tolk if this is being used on application

--- app/helpers/conductor/application_helper.rb
+++ app/helpers/conductor/application_helper.rb
@@ -2,6 +2,7 @@
 module Conductor::ApplicationHelper
 
   def tolk_link
+
     link_to_if(defined?(Tolk), "Tolk", '/tolk') do
       link_to("Tolk", "http://www.github.com/tolk/tolk",target: "_blank",confirm:"This extension is not installed." ) 
      end       
@@ -11,6 +12,8 @@ module Conductor::ApplicationHelper
     link_to_if(defined?(WebConsole), "WebConsole", '/console') do
       link_to("WebConsole", "https://github.com/rails/web-console",target: "_blank",confirm:"This extension is not installed." ) 
     end          
+  end
+      
   end
 
   def documentation_rails

f09093d394fb4c15c61dcf512926227344603c0b
lauragarcia <lauragarcia1993@hotmail.com>
2013-08-30 15:52:55 -0500

    Add a link to Tolk if this is being used on application

--- app/helpers/conductor/application_helper.rb
+++ app/helpers/conductor/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module Conductor::ApplicationHelper
+
+  def tolk_link
+    if defined?(Tolk)
+      link_to("Tolk", '/tolk') 
+    else
+      'Tolk'
+    end        
+  end
 end

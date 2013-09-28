04ac4ef69d1ee73c8839f132ece29eb7c2982a76
lauragarcia <lauragarcia1993@hotmail.com>
2013-09-28 16:38:04 -0500

    create test controller

---
+++ app/controllers/conductor/test_controller.rb
@@ -0,0 +1,10 @@
+require_dependency "conductor/application_controller"
+
+module Conductor
+  class TestController < ApplicationController
+  	def show
+  	end
+  	def run
+  	end
+  end
+end
---
+++ test/helpers/conductor/test_helper_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+module Conductor
+  class TestHelperTest < ActionView::TestCase
+  end
+end

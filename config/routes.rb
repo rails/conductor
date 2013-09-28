e588af1ecb920ea6cc75b10186ade7bbc1219e6e
lauragarcia <lauragarcia1993@hotmail.com>
2013-09-28 17:20:31 -0500

    runing test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Conductor::Engine.routes.draw do
   resources :scaffolds, :routes, :annotations, :statistics, :resources, :models, :app_controllers, :mailers
   resource :gemfile, :database
-   get "welcome/index"
+  
+  resource :test, only: [] do
+  	collection do
+  		get :run
+  		get :show
+  	end
+  end
+
+  get "welcome/index"
   resources :fixtures, except: [:edit, :update ,:show]
   get 'fixtures/*path', to: 'fixtures#edit', constraints: { path: /.*/ }
   patch 'fixtures/*path', to: 'fixtures#update', constraints: { path: /.*/ }

bfd09fff0cc6281240b0ed018c80c7514a18506f
lauragarcia <lauragarcia1993@hotmail.com>
2013-09-29 20:34:24 -0500

    readonly and delete line with numbers

--- app/assets/javascripts/conductor/tests.js
+++ app/assets/javascripts/conductor/tests.js
@@ -10,13 +10,12 @@ function doPoll(){
 $(document).ready(function(){
   var editor = ace.edit("editor");
   var textarea = $('#test_body');
+  editor.renderer.setShowGutter(false);
+  editor.setReadOnly(true);
   textarea.hide();
 
   editor.setTheme("ace/theme/textmate");
   editor.getSession().setMode("ace/mode/ruby");
   editor.getSession().setValue(textarea.val());
-  editor.getSession().on('change', function(){
-    textarea.val(editor.getSession().getValue());
-  });
-   doPoll();
+  doPoll();
 });

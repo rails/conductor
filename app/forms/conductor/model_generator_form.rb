0370d63c91ca4ee713c56101725636dea7402907
Guillermo Iguaran <guilleiguaran@gmail.com>
2013-10-01 16:42:23 -0500

    Fixes for skip_namespace option

--- app/forms/conductor/model_generator_form.rb
+++ app/forms/conductor/model_generator_form.rb
@@ -4,6 +4,6 @@ require 'rails/generators/rails/model/model_generator'
 module Conductor
   class ModelGeneratorForm < BaseGeneratorForm
     generator_class Rails::Generators::ModelGenerator
-    options :skip_timestamps, :skip_migration, :skip_namespaces, :skip_indexes, :skip_fixture
+    options :skip_timestamps, :skip_migration, :skip_namespace, :skip_indexes, :skip_fixture
   end
 end
--- app/forms/conductor/resource_generator_form.rb
+++ app/forms/conductor/resource_generator_form.rb
@@ -4,6 +4,6 @@ require 'rails/generators/rails/resource/resource_generator'
 module Conductor
   class ResourceGeneratorForm < BaseGeneratorForm
     generator_class Rails::Generators::ResourceGenerator
-    options :skip_timestamps, :skip_migration, :skip_namespaces, :skip_indexes, :skip_fixture, :skip_helper, :skip_assets
+    options :skip_timestamps, :skip_migration, :skip_namespace, :skip_indexes, :skip_fixture, :skip_helper, :skip_assets
   end
 end

a9929574c5f0856b3a99422dcec2db2ea29a4bb5
Kir Shatrov <shatrov@me.com>
2013-10-15 12:29:28 +0200

    Proper indentation and no warnings
    DEPRECATION WARNING: :confirm option is deprecated and will be removed from Rails 4.1. Use 'data: { confirm: 'Text' }' instead. (called from block in webconsole_link at conductor/app/helpers/conductor/application_helper.rb:12)

--- app/helpers/conductor/application_helper.rb
+++ app/helpers/conductor/application_helper.rb
@@ -3,37 +3,37 @@ module Conductor::ApplicationHelper
 
   def tolk_link
     link_to_if(defined?(Tolk), "Tolk", '/tolk') do
-      link_to("Tolk", "http://www.github.com/tolk/tolk",target: "_blank",confirm:"This extension is not installed." ) 
-     end       
+      link_to("Tolk", "http://www.github.com/tolk/tolk", target: "_blank", data: { confirm: "This extension is not installed." })
+     end
   end
 
   def webconsole_link
     link_to_if(defined?(WebConsole), "WebConsole", '/console') do
-      link_to("WebConsole", "https://github.com/rails/web-console",target: "_blank",confirm:"This extension is not installed." ) 
-    end          
+      link_to("WebConsole", "https://github.com/rails/web-console", target: "_blank", data: { confirm: "This extension is not installed." })
+    end
   end
 
   def documentation_rails
-    if File.exist?(File.join(Rails.root, 'doc', 'api','index.html'))
-      link_to_doc('Rails API','api')
+    if File.exist?(File.join(Rails.root, 'doc', 'api', 'index.html'))
+      link_to_doc('Rails API', 'api')
     else
-      link_to("Rails API", "http://api.rubyonrails.org/",target: "_blank")
+      link_to("Rails API", "http://api.rubyonrails.org/", target: "_blank")
     end
   end
-  
+
   def documentation_guides
-    if File.exist?(File.join(Rails.root, 'doc', 'guides','index.html'))
-      link_to_doc('Rails Guides','guides')
+    if File.exist?(File.join(Rails.root, 'doc', 'guides', 'index.html'))
+      link_to_doc('Rails Guides', 'guides')
     else
-      link_to("Rails Guides", "http://guides.rubyonrails.org/",target: "_blank")
+      link_to("Rails Guides", "http://guides.rubyonrails.org/", target: "_blank")
     end
   end
 
   def documentation_app
-    if File.exist?(File.join(Rails.root, 'doc','app','index.html'))
-      link_to_doc('Your Application','app')
+    if File.exist?(File.join(Rails.root, 'doc', 'app', 'index.html'))
+      link_to_doc('Your Application', 'app')
     else
-      link_to("Your Application","#",confirm:"rake doc:app generates documentation for your application in doc/app." ) 
+      link_to("Your Application","#", data: { confirm: "rake doc:app generates documentation for your application in doc/app." })
     end
   end
 

eac3fea5bee836592da97adbb0cadb7925e2442e
Guillermo Iguaran <guilleiguaran@gmail.com>
2013-12-11 18:42:16 -0500

    Add test for CodeStatistics

---
+++ test/models/code_statistics_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+module Conductor
+  class CodeStatisticsTest < ActiveSupport::TestCase
+    def setup
+      @stats = Conductor::CodeStatistics.new(['Models', 'app/models'], ['Controllers', 'app/controllers'])
+    end
+
+    def test_lines
+      models = @stats.lines.first
+
+      assert 'Models', models[:name]
+      assert 222, models[:lines]
+      assert 179, models[:codelines]
+      assert 7, models[:classes]
+      assert 26, models[:methods]
+      assert 3, models[:m_over_c]
+      assert 4, models[:loc_over_m]
+    end
+
+    def test_total_line
+      total = @stats.total_line
+
+      assert "Total", total[:name]
+      assert 527, total[:lines]
+      assert 428, total[:codelines]
+      assert 22, total[:classes]
+      assert 57, total[:methods]
+      assert 2, total[:m_over_c]
+      assert 5, total[:loc_over_m]
+    end
+  end
+end

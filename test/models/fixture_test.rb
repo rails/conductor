05520ddb5270b6111c569699f3b8c8465781e742
lauragarcia <lauragarcia1993@hotmail.com>
2013-12-10 20:27:22 -0500

    adding tests to fixtures

--- test/models/fixture_test.rb
+++ test/models/fixture_test.rb
@@ -6,5 +6,21 @@ module Conductor
       fixture = Conductor::Fixture.new(Rails.root.join("test", "fixtures","users.yml"))
       assert_match /firstname: MyString/, fixture.content
     end
+    def test_fixture_content_change
+    	fixture = Conductor::Fixture.new(Rails.root.join("test", "fixtures","users.yml"))
+    	content = fixture.content
+    	fixture.content = 'edit existing fixture'
+    	fixture.save
+    	assert_match /edit existing fixture/, fixture.content
+    	fixture.content = content
+    	fixture.save
+    end
+    def test_fixture_list
+    	list_test = Dir.chdir(Rails.root.join("test", "fixtures")) do
+        	         Dir[File.join("**","*.yml")]
+      		        end
+      list = Conductor::Fixture.all
+      assert_equal list_test , list
+    end
   end
 end

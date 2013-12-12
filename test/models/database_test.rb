b000074149aa90251488312c5a16049ac4030ceb
lauragarcia <lauragarcia1993@hotmail.com>
2013-12-11 22:36:57 -0500

    Fix database_test.rb

--- test/models/database_test.rb
+++ test/models/database_test.rb
@@ -3,16 +3,17 @@ require 'test_helper'
 module Conductor
   class DatabaseTest < ActiveSupport::TestCase
     def test_database 
-      assert_equal File.open(Rails.root.join('config','database.yml')).read , Database.instance.to_s
+      assert_match(/# SQLite version 3.x/, Database.instance.to_s)
     end
     def test_database_change
       database = Database.instance
       content = database.to_s
-      database.content = 'edit existing Database file'
+      database.content = '# edit existing Database file'
       database.save
-      assert_match /edit existing Database file/, database.content
-      database.content = content
-      database.save
+      assert_match(/# edit existing Database file/, database.content)
+    ensure
+      # restore the old content of the file
+      File.open(Rails.root.join("config","database.yml"), 'w') { |file| file.write(content) }
     end
   end
 end

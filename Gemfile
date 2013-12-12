b251929c155ea75c423fbae29cd6405015a6d586
lauragarcia <lauragarcia1993@hotmail.com>
2013-12-11 22:36:26 -0500

    Fix gemfile_test.rb

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
-source "http://rubygems.org"
+source "https://rubygems.org"
 
 # Declare your gem's dependencies in conductor.gemspec.
 # Bundler will treat runtime dependencies like base dependencies, and
--- test/models/gemfile_test.rb
+++ test/models/gemfile_test.rb
@@ -3,16 +3,17 @@ require 'test_helper'
 module Conductor
   class GemfileTest < ActiveSupport::TestCase
     def test_gemfile 
-      assert_equal File.open(Rails.root.join("Gemfile")).read , Gemfile.instance.to_s
+      assert_match(/"https:\/\/rubygems.org"/, Gemfile.instance.to_s)
     end
     def test_gemfile_change
       gemfile = Gemfile.instance
       content = gemfile.to_s
-      gemfile.content = 'edit existing Gemfile'
+      gemfile.content = '# edit existing Gemfile'
       gemfile.save
-      assert_match /edit existing Gemfile/, gemfile.content
-      gemfile.content = content
-      gemfile.save
+      assert_match(/# edit existing Gemfile/, gemfile.content)
+    ensure
+      # restore the old content of the file
+      File.open(Rails.root.join("Gemfile"), 'w') { |file| file.write(content) }
     end
   end
 end

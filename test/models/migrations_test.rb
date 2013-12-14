46f7e2f5f436827e64899bbadf22f6e25387c446
Guillermo Iguaran <guilleiguaran@gmail.com>
2013-12-13 20:17:11 -0500

    Add first test for Migrations

--- test/models/migrations_test.rb
+++ test/models/migrations_test.rb
@@ -2,10 +2,14 @@ require 'test_helper'
 
 module Conductor
   class MigrationTest < ActiveSupport::TestCase
-    def test_migrations
-      #ActiveRecord::Migrator.stub(:schema_migrations_table_name).returns("AnyName")
-      assert_raise (Conductor::Migrations::MigrationError) do 
-        Migrations.list
+    def test_migration_fail_when_schema_migration_does_not_exist
+      connection_mock = Object.new
+      def connection_mock.table_exists?(*) false end
+
+      ActiveRecord::Base.stub :connection, connection_mock do
+        assert_raise(Conductor::Migrations::MigrationError) do
+          Migrations.list
+        end
       end
     end
   end

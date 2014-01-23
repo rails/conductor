74d5369379755cc497b63222e1eaac0c3746840f
lauragarcia <lauragarcia1993@hotmail.com>
2014-01-23 17:07:59 -0500

    config.rb file for conductor

---
+++ lib/conductor/config.rb
@@ -0,0 +1,15 @@
+require 'active_support/configurable'
+
+module Conductor
+  include ActiveSupport::Configurable
+  config_accessor :enable_editor
+
+  def self.configure(&block)
+    yield self
+  end
+
+  
+  configure do |config|
+    config.enable_editor = true
+  end
+end
